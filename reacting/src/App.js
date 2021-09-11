3cc814128d8a73dd3a87872645b30a04946f2686
cyyan88 <cynthiayan88@gmail.com>
2021-09-11 10:41:44 -0400

    api call attempts

--- reacting/src/App.js
+++ reacting/src/App.js
@@ -1,7 +1,24 @@
 import {React, Component} from 'react'
-import Genius from 'genius-api'
-const accessToken = 'Tp_wXKpqqdlAfzZ9Fj_ql11TZk4Gc7_bPHxS7nQ4lCSa0tTqL75lRIRZFkrE2xf3'
-const genius = new Genius(accessToken)
+import axios from 'axios'
+
+const ACCESS_TOKEN = 'CsfADECm9bWsBConuB8CeeCPzM0xg-465U1ygw'
+
+let data
+const headers = {
+  'User-Agent': 'CompuServe Classic/1.22',
+  'Accept': 'application/json',
+  'Host': 'api.genius.com',
+  'Authorization': `Bearer ${ACCESS_TOKEN}`
+  //"Access-Control-Allow-Origin": "*"
+}
+const getSong = async () => {
+  //data= axios.get(`https://api.genius.com/search?q=Kendrick%20Lamar`, headers)
+  data = axios.get('https://my-json-server.typicode.com/nfried16/swe-practice/weather')
+  console.log(data)
+  
+  //console.log('getweather')
+}
+getSong()
 
 class App extends Component {
 
@@ -15,27 +32,9 @@ class App extends Component {
 
 
   render (){
-    Genius.prototype.getArtistIdByName = function getArtistIdByName(artistName) {
-      const normalizeName = name => name.replace(/\./g, '').toLowerCase()   // regex removes dots
-      const artistNameNormalized = normalizeName(artistName)
-    
-      return this.search(artistName)
-        .then((response) => {
-          for (let i = 0; i < response.hits.length; i += 1) {
-            const hit = response.hits[i]
-            if (hit.type === 'song' && normalizeName(hit.result.primary_artist.name) === artistNameNormalized) {
-              return hit.result
-            }
-          }
-          throw new Error(`Did not find any songs whose artist is "${artistNameNormalized}".`)
-        })
-        .then(songInfo => songInfo.primary_artist.id)
-    }
-    
-    const genius = new Genius(accessToken)
-    genius.getArtistIdByName('Drake')
-    .then(artistId => { /* ... */ })
-    .catch(err => console.error(err))
+
+  
+
     
     return(
       <div className="App">
